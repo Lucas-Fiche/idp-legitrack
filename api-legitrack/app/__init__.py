9c81d1e0302756f5ace8a3d71439bbae541017ac
Pedro Araujo <pedroredn@gmail.com>
2025-11-04 20:20:05 -0300

    Criação do BD e Implementação no Docker

--- api-legitrack/app/__init__.py
+++ api-legitrack/app/__init__.py
@@ -1,13 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 from .routes import bp
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     CORS(app)
+    db.init_app(app)          
+    migrate.init_app(app, db)
 
     # Importar e registrar rotas aqui
     app.register_blueprint(bp)
+
+    from . import models
 
     return app
---
+++ api-legitrack/app/models.py
@@ -0,0 +1,58 @@
+from . import db
+from datetime import datetime
+
+#TP
+class TP_Situacao(db.Model):
+    __tablename__ = 'tp_situacao'
+    id_situacao = db.Column(db.Integer, primary_key=True)
+    ds_situacao = db.Column(db.String(255), unique=True, nullable=False)
+
+class TP_Tramitacao(db.Model):
+    __tablename__ = 'tp_tramitacao'
+    id_tramitacao = db.Column(db.Integer, primary_key=True)
+    ds_tramitacao = db.Column(db.String(255), unique=True, nullable=False)
+
+class TP_Temas(db.Model):
+    __tablename__ = 'tp_temas'
+    id_tema = db.Column(db.Integer, primary_key=True)
+    ds_tema = db.Column(db.String(255), unique=True, nullable=False)
+
+#RL
+rel_temas = db.Table('rl_temas',
+    db.Column('id_rl_temas', db.Integer, primary_key=True),
+    db.Column('id_projeto', db.Integer, db.ForeignKey('tb_projeto.id_projeto')),
+    db.Column('id_tema', db.Integer, db.ForeignKey('tp_temas.id_tema'))
+)
+
+#TB
+class TB_Projeto(db.Model):
+    __tablename__ = 'tb_projeto'
+    
+    id_projeto = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    titulo_projeto = db.Column(db.String(500))
+    descricao = db.Column(db.Text)
+    ano_inicio = db.Column(db.Integer)
+    data_hora = db.Column(db.DateTime)
+    sigla_orgao = db.Column(db.String(100))
+    despacho = db.Column(db.Text)
+    id_ultima_situacao = db.Column(db.Integer, db.ForeignKey('tp_situacao.id_situacao'))
+    id_ultima_tramitacao = db.Column(db.Integer, db.ForeignKey('tp_tramitacao.id_tramitacao'))
+
+    tramitacoes = db.relationship('RL_Tramitacoes', backref='projeto', lazy='dynamic')
+    temas = db.relationship('TP_Temas', secondary=rel_temas, lazy='subquery',
+                            backref=db.backref('projetos', lazy=True))
+    ultima_situacao = db.relationship('TP_Situacao', foreign_keys=[id_ultima_situacao])
+    ultima_tramitacao = db.relationship('TP_Tramitacao', foreign_keys=[id_ultima_tramitacao])
+
+
+class RL_Tramitacoes(db.Model):
+    __tablename__ = 'rl_tramitacoes'
+    
+    id_rl_tramitacao = db.Column(db.Integer, primary_key=True)
+    id_projeto = db.Column(db.Integer, db.ForeignKey('tb_projeto.id_projeto'), nullable=False)
+    sequencia = db.Column(db.Integer, nullable=False)
+    id_situacao = db.Column(db.Integer, db.ForeignKey('tp_situacao.id_situacao'), nullable=False)
+    id_tramitacao = db.Column(db.Integer, db.ForeignKey('tp_tramitacao.id_tramitacao'), nullable=False)    
+    
+    situacao = db.relationship('TP_Situacao')
+    tipo_tramitacao = db.relationship('TP_Tramitacao')
