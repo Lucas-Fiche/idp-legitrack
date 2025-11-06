aa3fb8128e807ae69e7e901a07f78149652b9c17
Pedro Araujo <pedroredn@gmail.com>
2025-11-06 09:27:47 -0300

    Adicionando Script Criação de Schemas

---
+++ api-legitrack/app/schema_creator_script.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine, text
+
+engine = create_engine('postgresql://user:password@db:5432/legitrack_db')
+
+with engine.connect() as conn:
+    with conn.begin():
+        conn.execute(text("CREATE SCHEMA IF NOT EXISTS camara"))
+        print('Schema camara criado!')
+        conn.execute(text("CREATE SCHEMA IF NOT EXISTS senado"))
+        print('Schema senado criado!')
